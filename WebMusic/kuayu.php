7c63ace32157ae28fd5de60a1b08c0f495600a04
chenxing <chenxing1@travelsky.com>
2019-08-22 10:44:02 +0800

    搜索个数更改

--- WebMusic/kuayu.php
+++ WebMusic/kuayu.php
@@ -7,7 +7,7 @@
  */
 header('Content-type:text/json;charset=UTF-8');
 $Mname = $_POST['musicName'];
-$Url = 'http://music.163.com/api/search/get/web?type=1&limit=6&s=';
+$Url = 'http://music.163.com/api/search/get/web?type=1&limit=20&s=';
 $searchUrl = $Url.urlencode($Mname);
 $information = file_get_contents($searchUrl);
 echo $information;
